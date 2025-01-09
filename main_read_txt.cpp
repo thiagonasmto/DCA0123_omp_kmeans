37d2212cb34b22c821a1f1a076cd9c01df442ef1
PEDRO LEANDRO BATISTA MARQUES <plbmarques@service0.npad.internal>
2025-01-09 00:40:38 -0300

    mudança na função main_read_txt.cpp

--- main_read_txt.cpp
+++ main_read_txt.cpp
@@ -1,4 +1,6 @@
 #include "./lib/kMeansCalc.cpp"
+//#include "./pascal-releases-master/include/pascalops.h"
+//#include "pascalops.h"
 
 /*
 Here is an example of the primary way to use the kMeansCalc object;
@@ -7,19 +9,22 @@ Here is a main.cpp file to show how it works.
 */
 
 int main(int argc, char **argv){
-    int dimSize = 800;
+     if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <dimSize>\n";
+        return 1;
+    }
+
+    int dimSize = std::atoi(argv[1]); // Receber o tamanho do problema como argumento
     int clusterCount = 20;
-    int iterationCount = 1;
-    // optional parameter at end
-    // int numStart = 1;
+    int iterationCount = 1000;
     double threshold = 1;
+    
     kMeansCalc<double> k {"./data/mnist_train.txt",dimSize};
     std::cout << "Avg fitness: " << k.doubleFindOptimalClusters(clusterCount,iterationCount,threshold,1) << "\n";
 
     k.voidWritePointsToFile("out.txt");
-    k.voidPrintSummary();
-    k.voidPrintClusterSummary();
-
+    //k.voidPrintSummary();
+    //k.voidPrintClusterSummary();
 
     return 0;
 }
